12f8cfc22fe54e0ad8624615f738883217ce551c
Manthan Khandale <manthankhandale@gmail.com>
2022-05-22 21:47:11 +0530

    flutter 3 support

--- android/app/src/main/java/site/manthan/atom_lab/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package site.manthan.atom_lab;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/atom_lab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atom_lab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
