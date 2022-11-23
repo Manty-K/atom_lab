9c4bbad67cab68dae0fac58fea968ea2b424c68e
Manthan Khandale <manthan@kisan.net>
2022-11-23 15:46:00 +0530

    ui changes

--- lib/screens/element_info_screen/element_info.dart
+++ lib/screens/element_info_screen/element_info.dart
@@ -68,232 +68,230 @@ class ElementInfo extends StatelessWidget {
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
           ))),
-          SafeArea(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                symbol(index),
-                                style: kSymbolStyle,
-                              ),
-                              Text(
-                                atomicNumber(index),
-                                style: kAtomicNumberStyle,
-                              ),
-                            ],
-                          ),
-                          Text(
-                            name(index),
-                            style: kNameStyle(elementColor),
-                          )
-                        ],
-                      ),
-                      Text(
-                        groupBlock(index),
-                        style: kAtomicNumberStyle,
-                      )
-                    ],
-                  ),
-                  Spacer(),
-                  AtomAnimation(index),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'ATOMIC MASS',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            atomicMass(index) == ''
-                                ? '--'
-                                : atomicMass(index) + ' u',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'ATOMIC RADIUS',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            atomicRadius(index) == ''
-                                ? '--'
-                                : atomicRadius(index) + ' pm',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Column(
-                    children: [
-                      Text(
-                        'ELECTRON CONFIGURATION',
-                        style: kPropertyStyle,
-                      ),
-                      Text(
-                        electronConfiguration(index) == ''
-                            ? '--'
-                            : electronConfiguration(index),
-                        style: kNameStyle(elementColor),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'OXIDATION STATE',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            oxidationStates(index) == ''
-                                ? '--'
-                                : oxidationStates(index),
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'ELECTRONEGATIVITY',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            electronegativity(index) == ''
-                                ? '--'
-                                : electronegativity(index),
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'IONIZATION ENERGY',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            ionizationEnergy(index) == ''
-                                ? '--'
-                                : ionizationEnergy(index) + ' ev',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'ELECTRON AFFINITY',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            electronAffinity(index) == ''
-                                ? '--'
-                                : electronAffinity(index) + ' ev',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'MELTING POINT',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            meltingPoint(index) == ''
-                                ? '--'
-                                : meltingPoint(index) + ' K',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'BOILING POINT',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            boilingPoint(index) == ''
-                                ? '--'
-                                : boilingPoint(index) + ' K',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'DENSITY',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            density(index) == ''
-                                ? '--'
-                                : density(index) + ' g/cm²',
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'STANDARD STATE',
-                            style: kPropertyStyle,
-                          ),
-                          Text(
-                            standardState(index) == ''
-                                ? '--'
-                                : standardState(index),
-                            style: kNameStyle(elementColor),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                ],
-              ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              symbol(index),
+                              style: kSymbolStyle,
+                            ),
+                            Text(
+                              atomicNumber(index),
+                              style: kAtomicNumberStyle,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          name(index),
+                          style: kNameStyle(elementColor),
+                        )
+                      ],
+                    ),
+                    Text(
+                      groupBlock(index),
+                      style: kAtomicNumberStyle,
+                    )
+                  ],
+                ),
+                Spacer(),
+                AtomAnimation(index),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'ATOMIC MASS',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          atomicMass(index) == ''
+                              ? '--'
+                              : atomicMass(index) + ' u',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'ATOMIC RADIUS',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          atomicRadius(index) == ''
+                              ? '--'
+                              : atomicRadius(index) + ' pm',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    Text(
+                      'ELECTRON CONFIGURATION',
+                      style: kPropertyStyle,
+                    ),
+                    Text(
+                      electronConfiguration(index) == ''
+                          ? '--'
+                          : electronConfiguration(index),
+                      style: kNameStyle(elementColor),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'OXIDATION STATE',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          oxidationStates(index) == ''
+                              ? '--'
+                              : oxidationStates(index),
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'ELECTRONEGATIVITY',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          electronegativity(index) == ''
+                              ? '--'
+                              : electronegativity(index),
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'IONIZATION ENERGY',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          ionizationEnergy(index) == ''
+                              ? '--'
+                              : ionizationEnergy(index) + ' ev',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'ELECTRON AFFINITY',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          electronAffinity(index) == ''
+                              ? '--'
+                              : electronAffinity(index) + ' ev',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'MELTING POINT',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          meltingPoint(index) == ''
+                              ? '--'
+                              : meltingPoint(index) + ' K',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'BOILING POINT',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          boilingPoint(index) == ''
+                              ? '--'
+                              : boilingPoint(index) + ' K',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'DENSITY',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          density(index) == ''
+                              ? '--'
+                              : density(index) + ' g/cm²',
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'STANDARD STATE',
+                          style: kPropertyStyle,
+                        ),
+                        Text(
+                          standardState(index) == ''
+                              ? '--'
+                              : standardState(index),
+                          style: kNameStyle(elementColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Spacer(),
+              ],
             ),
           )
         ],
