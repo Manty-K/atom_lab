d4f0c7f498a6bbbfa4655220d9d5f19104206083
Manthan Khandale <manthan@kisan.net>
2022-11-23 16:28:14 +0530

    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Atom Lab',
       theme: ThemeData(
         scaffoldBackgroundColor: kBlack,
         visualDensity: VisualDensity.adaptivePlatformDensity,
